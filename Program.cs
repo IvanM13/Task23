f99902b6f54e5f3a7caccbcf34d0a3825e789aea
Ivan M <slade91@mail.ru>
2022-10-16 01:08:25 +0300

    second

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
+
+void GetNumb(int number)
+{
+    Console.Write($"{number} -> ");
+    for (int i = 1; i <= number; i++)
+    {
+        double cube = Math.Pow(i, 3);
+        Console.Write($"{cube}, ");
+    }
+}
+Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+GetNumb(numb);
